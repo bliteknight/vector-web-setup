22880ea352fd91f6a80acf29875aeeed7560bfa8
root <bliteknight@gmail.com>
2023-08-28 12:20:51 -0500

    Initial checkin

--- CONTRIBUTORS.md
+++
@@ -1,7 +0,0 @@
-# Contributors
-
-In addition to the staff at Anki and Digital Dream Labs, the following people have provided code to improve the software:
-
-* Lee Alexis
-* Gavin Hamill
-* Anant Kaushik
--- test/vector-setup/index.js
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) 2019-2020 Digital Dream Labs. See LICENSE file for details. */
-
-"use strict";
-
-describe("The vector-web-setup end to end", () => {
-  require("./addOta.e2e")();
-  require("./approveOta.e2e")();
-});
